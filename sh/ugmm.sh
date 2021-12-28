19d559c277888e8260801bdd63c002db1f90ab0e
dee-lii <ggggdl@live.cn>
2021-12-28 10:08:25 +0800

    sh

--- sh/ugmm.sh
+++ sh/ugmm.sh
@@ -1,4 +1,59 @@
 #!/bin/bash
+
+green(){
+    echo -e "\033[32m\033[01m$1\033[0m"
+}
+
+red(){
+    echo -e "\033[31m\033[01m$1\033[0m"
+}
+
+yellow(){
+    echo -e "\033[33m\033[01m$1\033[0m"
+}
+
+if [[ -f /etc/redhat-release ]]; then
+release="Centos"
+elif cat /etc/issue | grep -q -E -i "debian"; then
+release="Debian"
+elif cat /etc/issue | grep -q -E -i "ubuntu"; then
+release="Ubuntu"
+elif cat /etc/issue | grep -q -E -i "centos|red hat|redhat"; then
+release="Centos"
+elif cat /proc/version | grep -q -E -i "debian"; then
+release="Debian"
+elif cat /proc/version | grep -q -E -i "ubuntu"; then
+release="Ubuntu"
+elif cat /proc/version | grep -q -E -i "centos|red hat|redhat"; then
+release="Centos"
+else 
+red "不支持你当前系统，请选择使用Ubuntu,Debian,Centos系统。"
+rm -f root.sh
+exit 1
+fi
+
+if ! type sudo >/dev/null 2>&1; then 
+yellow "检测到sudo未安装，安装中 "
+if [ $release = "Centos" ]; then
+yum -y update && yum install sudo -y
+else
+apt-get update -y && apt-get install sudo -y
+fi	   
+else
+green "sudo已安装"
+fi
+
+sudo lsattr /etc/passwd /etc/shadow >/dev/null 2>&1
+sudo chattr -i /etc/passwd /etc/shadow >/dev/null 2>&1
+sudo chattr -a /etc/passwd /etc/shadow >/dev/null 2>&1
+sudo lsattr /etc/passwd /etc/shadow >/dev/null 2>&1
+
+green "VPS设置root密码登录脚本"
+read -p "设置root密码:" password
+echo root:$password | sudo chpasswd root
 sudo sed -i 's/^#\?PermitRootLogin.*/PermitRootLogin yes/g' /etc/ssh/sshd_config;
 sudo sed -i 's/^#\?PasswordAuthentication.*/PasswordAuthentication yes/g' /etc/ssh/sshd_config;
-systemctl restart sshd.service
+sudo service sshd restart
+green "VPS用户名：root"
+green "vps密码：$password"
+echo "请妥善保存好登录信息！然后重启VPS确保设置已保存！"
